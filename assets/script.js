066dd606ba1ef765f44dac75adcad67878233304
Ashish Chaudhary <201731753+AshishChaudhary01@users.noreply.github.com>
2025-05-10 15:27:00 +0545

    Complete project upto foundations course
    Complete project upto foundations course requirements.

---
+++ assets/script.js
@@ -0,0 +1,111 @@
+  let humanScore = 0;
+  let computerScore = 0;
+
+function getComputerChoice(){
+  let randomNumber = Math.random();
+
+  if(randomNumber < 0.33){
+    return "rock";
+  }
+  else if(randomNumber > 0.33 && randomNumber < 0.67){
+    return "paper";
+  }
+  else{
+    return "scissor";
+  }
+}
+
+function getHumanChoice(){
+  return prompt("Enter your choice in Lower case\n'rock' 'paper' 'scissor'", "rock");
+
+}
+
+function decideWinner(humanChoice, computerChoice){
+  let winner = "";
+  if(humanChoice === "rock"){
+    if(computerChoice === "paper"){
+      winner = "computer";
+    }else if(computerChoice === "scissor"){
+      winner = "human";
+    }
+    else
+      winner = "draw"
+  }
+
+  if(humanChoice === "paper"){
+    if(computerChoice === "scissor"){
+      winner = "computer";
+    }else if(computerChoice === "rock"){
+      winner = "human";
+    }
+    else
+      winner = "draw"
+  }
+
+  if(humanChoice === "scissor"){
+    if(computerChoice === "rock"){
+      winner = "computer";
+    }else if(computerChoice === "paper"){
+      winner = "human";
+    }
+    else
+      winner = "draw"
+  }
+
+  return winner;
+}
+
+function updateScore(winner){
+  if(winner == "human"){
+    humanScore += 1;
+  }
+  if(winner == "computer"){
+    computerScore += 1;
+  }
+}
+
+function showRoundResult(winner, humanChoice, computerChoice){
+  let result = "";
+    if(winner === "human"){
+      result =  `You win! ${humanChoice} beats ${computerChoice}`;
+    }else if(winner === "computer"){
+      result = `You lose! ${computerChoice} beats ${humanChoice}`;
+    }
+    else{
+      result = `Draw! both chose ${humanChoice}`;
+    }
+  console.log(result);
+}
+
+function showFinalResult(){
+  if(humanScore>computerScore){
+    console.log(`You won! your score: ${humanScore} computer score: ${computerScore}`)
+  }
+  if(humanScore<computerScore){
+    console.log(`You Lost! your score: ${humanScore} computer score: ${computerScore}`)
+  }
+  else{
+    console.log(`Draw! your score: ${humanScore} computer score: ${computerScore}`)
+  }
+}
+
+function playRound(){
+  let humanChoice = getHumanChoice();
+  let computerChoice = getComputerChoice();
+  
+  humanChoice = humanChoice.toLowerCase();
+  let winner = decideWinner(humanChoice, computerChoice);
+  updateScore(winner);
+  showRoundResult(winner, humanChoice, computerChoice);
+}
+
+function playGame(){
+  //Play 5 rounds
+  for(let i=0; i<5; i++){
+    playRound();
+  }
+  showFinalResult();
+}
+
+//Run the Program/Game
+playGame();
